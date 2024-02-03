938201385dcb027bc859a0164a66266a5cd9b6df
Art7578 <113849803+Art7578@users.noreply.github.com>
2024-02-03 16:44:44 +0200

    add shopping cart

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import css from './Header.module.css'
 
 export default function Header() {
+  let [cartActive, setCartActive] = useState(false);
+
   return (
-    <header>
+    <header className={css.header}>
       <div>
         <span className={css.logo}>Games</span>
         <ul className={css.nav}>
           <li className={css.nav_item}>About us</li>
           <li className={css.nav_item}>Contacts</li>
         </ul>
+        <span onClick={() => setCartActive(cartActive = !cartActive)} className={`${css.cart_btn} ${cartActive && css.active}`}>🛒</span>
+
+        {cartActive && (
+          <div className={css.shop_cart}>
+
+          </div>
+        )}
       </div>
       <div className={css.presentation}></div>
     </header>
